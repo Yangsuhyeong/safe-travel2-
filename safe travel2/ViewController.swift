19c9ac7da004305e1225a759ae05139750073566
irishev <cscoi015@cscoi015ui-iMac.local>
2017-01-11 17:59:39 +0900

    changes

---
+++ safe travel2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  safe travel2
+//
+//  Created by cscoi015 on 2017. 1. 6..
+//  Copyright © 2017년 doken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ safe travel2Tests/safe_travel2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  safe_travel2Tests.swift
+//  safe travel2Tests
+//
+//  Created by cscoi015 on 2017. 1. 6..
+//  Copyright © 2017년 doken. All rights reserved.
+//
+
+import XCTest
+@testable import safe_travel2
+
+class safe_travel2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
